ade764749a25ab6685fc968e55d0d02ece3e813e
iambatman <iambatman@kali>
2026-05-30 10:14:12 -0400

    Initial ToDoApp

---
+++ OnlineTodo/ToDoApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpRequest
+from .models import Todoapp
+# Create your views here.
+def todo_list(request):
+    all_data = {'todo_list':Todoapp.objects.all()}
+    return render(request,'ToDoApp/todo_list.html',all_data)
+def insert_todo(request:HttpRequest):
+    todo=Todoapp(content=request.POST['content'])
+    todo.save()
+    return redirect('/todo/list')
+
+def delete_todo_item(request,todo_id):
+    delete_id=Todoapp.objects.get(id=todo_id)
+    delete_id.delete()
+    return redirect('/todo/list')
